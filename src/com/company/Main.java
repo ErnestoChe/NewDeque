fd5e5330671cef7c6615275a3cdbcfd6fb82be1e
Ernest <ernestfilippov@mail.ru>
2019-02-17 15:20:36 +0300

    upd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,6 @@
 package com.company;
 import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-
-}
 class Deque<T>
 {
     private ArrayList<T> list;
